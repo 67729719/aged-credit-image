476d6ed637050d08f861457ce89da8304281cc6b
KevinYu <67729719@qq.com>
2025-04-16 12:42:09 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "cyberpunk Garden, dog, cat, person",
+      prompt: "cyberpunk A man smoking in the garden",
     };
 
     const response = await env.AI.run(
